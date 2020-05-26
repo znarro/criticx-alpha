f1ad47008ffb227ab993bb6c5f37700568393750
Josue Narro <josue.narro.a@gmail.com>
2020-05-25 23:34:13 -0500

    Create Platform model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_043301) do
+ActiveRecord::Schema.define(version: 2020_05_26_043353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2020_05_26_043301) do
     t.date "release_date"
     t.integer "category"
     t.decimal "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "name"
+    t.integer "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
